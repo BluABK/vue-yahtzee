3197d6d1e858c0acaf8da23d990db5489b323592
BluABK <bluabk@bluabk.net>
2021-04-30 18:32:13 +0200

    Made board table cell font cursive and code fixup

--- src/components/BoardRow.vue
+++ src/components/BoardRow.vue
@@ -1,8 +1,8 @@
 <template>
-  <tr class="row" :class="thickBottomBorder && 'thick-bottom-border'">
+  <tr class="board-row" :class="thickBottomBorder && 'thick-bottom-border'">
     <th class="board-col">{{ description }}</th>
     <template v-for="column in columns">
-      <td class="board-col" v-bind:key="column"></td>
+      <td class="board-col" :key="column"></td>
     </template>
   </tr>
 </template>
@@ -25,12 +25,6 @@ export default {
 
 .board-col {
   min-width: 30px;
-  /*border: 1px solid black;*/
-  /*background-color: #57ff40;*/
-}
-
-.board-row {
-  border: 1px solid black;
 }
 
 .thick-bottom-border {
@@ -38,7 +32,7 @@ export default {
 }
 
 .board-table td {
-  /*border: 1px solid black;*/
   border-left: 1px solid black;
+  font-family: cursive;
 }
 </style>
